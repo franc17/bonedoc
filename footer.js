990fdf26127cf867c3390ea2ca1be263d9895adb
franc17 <58270519+franc17@users.noreply.github.com>
2022-02-14 16:53:27 +0000

    React components

--- footer.js
+++ footer.js
@@ -1,67 +1,33 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+class Footer extends React.Component {
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+  render() {
+    return (
+    <div className="footer">
+        <div className="footer-div">
+            <p>
+                For private patient enquiries please contact Joanne Dobson
+            </p>
+            <p>
+                020 8527 0977 | jawdobson@gmail.com
+            </p>
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+        </div>
+        <div className="footer-div">
+            <p>
+                BLRS logo reproduced by kind permission of the BLRS Secretary.
+            </p>
+            <p>
+                Site designed and developed by <a href="https://www.francesjacksonbarry.com/" target="__blank">Frances Jackson-Barry</a>.
+            </p>
+        </div>
+    </div>
+    );
+  }
+}
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var Footer = function (_React$Component) {
-    _inherits(Footer, _React$Component);
-
-    function Footer() {
-        _classCallCheck(this, Footer);
-
-        return _possibleConstructorReturn(this, (Footer.__proto__ || Object.getPrototypeOf(Footer)).apply(this, arguments));
-    }
-
-    _createClass(Footer, [{
-        key: "render",
-        value: function render() {
-            return React.createElement(
-                "div",
-                { className: "footer" },
-                React.createElement(
-                    "div",
-                    { className: "footer-div" },
-                    React.createElement(
-                        "p",
-                        null,
-                        "For private patient enquiries please contact Joanne Dobson"
-                    ),
-                    React.createElement(
-                        "p",
-                        null,
-                        "020 8527 0977 | jawdobson@gmail.com"
-                    )
-                ),
-                React.createElement(
-                    "div",
-                    { className: "footer-div" },
-                    React.createElement(
-                        "p",
-                        null,
-                        "BLRS logo reproduced by kind permission of the BLRS Secretary."
-                    ),
-                    React.createElement(
-                        "p",
-                        null,
-                        "Site designed and developed by ",
-                        React.createElement(
-                            "a",
-                            { href: "https://www.francesjacksonbarry.com/", target: "__blank" },
-                            "Frances Jackson-Barry"
-                        ),
-                        "."
-                    )
-                )
-            );
-        }
-    }]);
-
-    return Footer;
-}(React.Component);
-
-ReactDOM.render(React.createElement(Footer, null), document.getElementById("footer-container"));
+ReactDOM.render(
+  <Footer />,
+  document.getElementById("footer-container")
+);
--- navbar.js
+++ navbar.js
@@ -1,148 +1,37 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+class Navigation extends React.Component {
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var Navigation = function (_React$Component) {
-  _inherits(Navigation, _React$Component);
-
-  function Navigation() {
-    _classCallCheck(this, Navigation);
-
-    return _possibleConstructorReturn(this, (Navigation.__proto__ || Object.getPrototypeOf(Navigation)).apply(this, arguments));
+  render() {
+    return (
+      <div>
+          <nav id="navholder" className="navbar">
+            <ul id="navitems">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="practice.html">Practice</a></li>
+                <li><a href="fees.html">Fees</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+          </nav>
+          <nav id="mobile-menu-holder" className="navbar">
+            <div>
+              <button id="mobile-menu-button" onClick="showMenu()"> &#9776; </button>
+                <ul id="mobile-navitems">
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="about.html">About</a></li>
+                  <li><a href="practice.html">Practice</a></li>
+                  <li><a href="fees.html">Fees</a></li>
+                  <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+      </div>
+    );
   }
+}
 
-  _createClass(Navigation, [{
-    key: "render",
-    value: function render() {
-      return React.createElement(
-        "div",
-        null,
-        React.createElement(
-          "nav",
-          { id: "navholder", className: "navbar" },
-          React.createElement(
-            "ul",
-            { id: "navitems" },
-            React.createElement(
-              "li",
-              null,
-              React.createElement(
-                "a",
-                { href: "index.html" },
-                "Home"
-              )
-            ),
-            React.createElement(
-              "li",
-              null,
-              React.createElement(
-                "a",
-                { href: "about.html" },
-                "About"
-              )
-            ),
-            React.createElement(
-              "li",
-              null,
-              React.createElement(
-                "a",
-                { href: "practice.html" },
-                "Practice"
-              )
-            ),
-            React.createElement(
-              "li",
-              null,
-              React.createElement(
-                "a",
-                { href: "fees.html" },
-                "Fees"
-              )
-            ),
-            React.createElement(
-              "li",
-              null,
-              React.createElement(
-                "a",
-                { href: "contact.html" },
-                "Contact"
-              )
-            )
-          )
-        ),
-        React.createElement(
-          "nav",
-          { id: "mobile-menu-holder", className: "navbar" },
-          React.createElement(
-            "div",
-            null,
-            React.createElement(
-              "button",
-              { id: "mobile-menu-button", onClick: "showMenu()" },
-              " \u2630 "
-            ),
-            React.createElement(
-              "ul",
-              { id: "mobile-navitems" },
-              React.createElement(
-                "li",
-                null,
-                React.createElement(
-                  "a",
-                  { href: "index.html" },
-                  "Home"
-                )
-              ),
-              React.createElement(
-                "li",
-                null,
-                React.createElement(
-                  "a",
-                  { href: "about.html" },
-                  "About"
-                )
-              ),
-              React.createElement(
-                "li",
-                null,
-                React.createElement(
-                  "a",
-                  { href: "practice.html" },
-                  "Practice"
-                )
-              ),
-              React.createElement(
-                "li",
-                null,
-                React.createElement(
-                  "a",
-                  { href: "fees.html" },
-                  "Fees"
-                )
-              ),
-              React.createElement(
-                "li",
-                null,
-                React.createElement(
-                  "a",
-                  { href: "contact.html" },
-                  "Contact"
-                )
-              )
-            )
-          )
-        )
-      );
-    }
-  }]);
-
-  return Navigation;
-}(React.Component);
-
-ReactDOM.render(React.createElement(Navigation, null), document.getElementById("navbar-container"));
+ReactDOM.render(
+  <Navigation />,
+  document.getElementById("navbar-container")
+);
